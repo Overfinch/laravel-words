c6fbaf69679f6195cbba38f363c783054908b789
Overfinch <bundichmail@gmail.com>
2023-02-07 01:52:06 +0200

    make migration,model,seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Word;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,5 +21,21 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+//        for ($i = 1; $i <= 20; $i++){
+//            Word::create([
+//                 'word' => fake()->word(),
+//                 'turn' => $i,
+//                 'player_id' => 1
+//             ]);
+//
+//            Word::create([
+//                'word' => fake()->word(),
+//                'turn' => $i,
+//                'player_id' => 2
+//            ]);
+//        }
+
+
     }
 }
